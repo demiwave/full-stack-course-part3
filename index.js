d516a35426445e65d13353af497fd81b5d6d8de3
mdan22 <mdan22@proton.me>
2024-07-16 14:33:41 +0200

    add solution for 3.3

--- index.js
+++ index.js
@@ -4,24 +4,24 @@ app.use(express.json())
 
 let persons = [
     { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
+      id: 1,
+      name: "Arto Hellas", 
+      number: "040-123456"
     },
     { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
+      id: 2,
+      name: "Ada Lovelace", 
+      number: "39-44-5323523"
     },
     { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
+      id: 3,
+      name: "Dan Abramov", 
+      number: "12-43-234345"
     },
     { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
+      id: 4,
+      name: "Mary Poppendieck", 
+      number: "39-23-6423122"
     }
 ]
 
@@ -40,6 +40,20 @@ app.get( '/api/info', (request, response) => {
     response.send(
         `<p>Phonebook has info for ${numberOfEntries} people<br/><br/>${currentDate}</p>`
     )
+})
+
+// get request: get data of specific person by id
+app.get('/api/persons/:id', (request, response) => {
+    // Using Number() isn't necessary but it's more efficient and it may lead to errors otherwise
+    const id = Number(request.params.id)
+    // find the person by id
+    const person = persons.find(person => person.id === id)
+    if(person) {
+        response.json(person)
+    }
+    else {
+        response.status(404).end() // the requested data was not found
+    }
 })
 
 const PORT = 3001
